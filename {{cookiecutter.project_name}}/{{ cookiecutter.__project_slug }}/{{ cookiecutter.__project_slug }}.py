63bc1a077ae719d4a7412b7bf6830175b1bc7890
Mike Salvatore <mike.s.salvatore@gmail.com>
2025-03-15 22:11:16 -0400

    Add ocp_vscode.show_all import

--- {{cookiecutter.project_name}}/{{ cookiecutter.__project_slug }}/{{ cookiecutter.__project_slug }}.py
+++ {{cookiecutter.project_name}}/{{ cookiecutter.__project_slug }}/{{ cookiecutter.__project_slug }}.py
@@ -1,4 +1,12 @@
 from build123d import *
-from ocp_vscode import get_defaults, reset_show, set_defaults, set_port, show, show_object
+from ocp_vscode import (  # noqa: F401
+    get_defaults,
+    reset_show,
+    set_defaults,
+    set_port,
+    show,
+    show_all,
+    show_object,
+)
 
 set_port(3939)
